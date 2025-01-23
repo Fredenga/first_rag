06da9c1ac9dbbf71be80b9b5088cc620300c8ed2
Fredrick Denga <dengafred@gmail.com>
2025-01-23 06:48:40 -0800

    load documents2

--- populate_database.py
+++ populate_database.py
@@ -10,3 +10,25 @@ from langchain.vectorstores.chroma import Chroma
 
 CHROMA_PATH = "chroma"
 DATA_PATH = "data"
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--reset", action="store_true", help="Reset the database.")
+    args = parser.parse_args()
+
+    if args.reset:
+        print("😎, Clearing the database")
+        # clear_database()
+
+    # create or update the datastore
+    # documents = load_documents()
+    # chunks = split_documents(documents)
+    # add_to_chroma(chunks)
+
+def load_documents():
+    document_loader = PyPDFDirectoryLoader(DATA_PATH)
+    return document_loader.load()
+
+def split_documents(documents: list[Document]):
+    pass
+
