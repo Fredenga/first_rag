41a41dd8093c30bf9722d556545709aae7dc604b
Fredrick Denga <dengafred@gmail.com>
2025-01-24 10:30:20 -0800

    split document into chunks feature

--- rag2/chunk_vector_store.py
+++ rag2/chunk_vector_store.py
@@ -1,2 +1,22 @@
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.vectorstores.utils import filter_complex_metadata
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_community.vectorstores import Chroma
+from langchain_community.embeddings import fastembed
+
 class ChunkVectorStore:
-    pass
+
+    def split_into_chunks(self, file_path: str):
+        doc = PyPDFLoader.load(file_path)
+        text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size=1024,
+            chunk_overlap=20
+        )
+
+        chunks = text_splitter.split_documents(doc)
+        chunks = filter_complex_metadata(chunks)
+
+        return chunks
+
+    def store_to_vector_database(self):
+        pass
