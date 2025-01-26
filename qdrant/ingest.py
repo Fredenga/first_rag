c3081519f656cfa9a8758fda7b8ec5b471203a3b
Fredrick Denga <dengafred@gmail.com>
2025-01-26 11:39:06 -0800

    setup Qdrant vector database

---
+++ qdrant/ingest.py
@@ -0,0 +1,6 @@
+from langchain.vectorstores import Qdrant
+from langchain.embeddings import HuggingFaceBgeEmbeddings
+from langchain.document_loaders import PyPDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+
+loader = PyPDFLoader()
