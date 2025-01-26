4f17c2285fa0634dfeb5d5a1c94ce0d617dbb9a1
Fredrick Denga <dengafred@gmail.com>
2025-01-26 12:02:51 -0800

    write flow

--- qdrant/ingest.py
+++ qdrant/ingest.py
@@ -12,3 +12,26 @@ text_splitter = RecursiveCharacterTextSplitter(
 )
 
 texts = text_splitter.split_documents(documents)
+
+# Load the embedding model
+model_name = "BAAI/bge-large-en"
+model_kwargs = {"device": "cpu"}
+encode_kwargs = {'normalize_embeddings': False}
+
+embeddings = HuggingFaceBgeEmbeddings(
+    model_name=model_name,
+    model_kwargs=model_kwargs,
+    encode_kwargs=encode_kwargs
+)
+
+url = "http://localhost:6333"
+collection_name = "gpt_db"
+
+x = Qdrant.from_documents(
+    texts,
+    embeddings,
+    url = url,
+    prefer_gprc = False,
+    collection_name = collection_name
+)
+
