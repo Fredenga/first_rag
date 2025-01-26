ec469bf8180b7363b2eb07e802d28c9e9402132b
Fredrick Denga <dengafred@gmail.com>
2025-01-26 11:42:02 -0800

    load and split documents

--- qdrant/ingest.py
+++ qdrant/ingest.py
@@ -3,4 +3,12 @@ from langchain.embeddings import HuggingFaceBgeEmbeddings
 from langchain.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 
-loader = PyPDFLoader()
+loader = PyPDFLoader("./data/data.pdf")
+
+documents = loader.load()
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size = 500,
+    chunk_overlap = 50
+)
+
+texts = text_splitter.split_documents(documents)
