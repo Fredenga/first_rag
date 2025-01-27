fbd462232466ee1f0af1021fd44bd7e5a17818c0
Fredrick Denga <dengafred@gmail.com>
2025-01-27 05:43:51 -0800

    Integrate chat with command line

--- langchain/main.py
+++ langchain/main.py
@@ -13,10 +13,33 @@ model = ChatMistralAI(api_key=api_key)
 # Gives broad context to the conversation (SystemMessage)
 # Message from Human to AI model (HumanMessage)
 # AI responding back to us (AIMessage)
-messages = [
-    SystemMessage(content="Answer this history questions"),
-    HumanMessage(content="Why was Japan not split into two like Germany")
-]
 
-result = model.invoke(messages)
-print(f"Answer from AI {result.content}")
+# SystemMessage -> HumanMessage -> AIMessage
+
+# messages = [
+#     SystemMessage(content="Answer this history questions"),
+#     HumanMessage(content="Why was Japan not split into two like Germany")
+# ]
+
+# result = model.invoke(messages)
+# print(f"Answer from AI {result.content}")
+
+chat_history = []
+system_message = SystemMessage(content="You are a helpful AI assistant")
+chat_history.append(system_message)
+
+while True:
+    query = input("You: ")
+    if query.lower() == "exit":
+        break
+
+    chat_history.append(HumanMessage(content=query)) # Add user message
+
+    result = model.invoke(chat_history)
+    response = result.content
+
+    chat_history.append(AIMessage(content=response))
+    print(f"AI {response}")
+    
+print("-----------Message History-----------")
+print(chat_history)
