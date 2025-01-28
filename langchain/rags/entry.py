fec5db34bf1767d0f8511ad970e555b6084594e5
Fredrick Denga <dengafred@gmail.com>
2025-01-28 12:57:18 -0800

    load documents and setup embeddings

---
+++ langchain/rags/entry.py
@@ -0,0 +1,38 @@
+import os
+from dotenv import load_dotenv
+from langchain_mistralai import MistralAIEmbeddings
+from langchain_community.document_loaders import PyPDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+
+load_dotenv()
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+file_path = os.path.join(current_dir, "data", "Database_Systems.pdf")
+
+if not os.path.exists(file_path):
+    raise FileNotFoundError(
+        f"The file {file_path} does not exist. Please check the path"
+    )
+# Load documents
+loader = PyPDFLoader(file_path=file_path)
+documents = loader.load()
+
+# Split documents into chunks
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=1000,
+    chunk_overlap=50
+)
+
+docs = text_splitter.split_documents(documents=documents)
+# Load embeddings
+ 
+import os
+
+def load_embeddings():
+    api_key = os.getenv("MISTRAL_API_KEY")
+
+    embeddings = MistralAIEmbeddings(api_key=api_key)
+
+    return embeddings
+
+embeddings = load_embeddings()
